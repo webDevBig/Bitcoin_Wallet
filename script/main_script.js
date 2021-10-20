e068806f2538ca945b6324343976ac02b62e5a5f
Olena Kushnir <olenadev7@gmail.com>
2021-10-20 15:20:54 +0300

    add new

--- script/main_script.js
+++ script/main_script.js
@@ -36,21 +36,105 @@ $(".toggle-password").click(function () {
     }
 });
 
-
-$('.history').click(function () {
-
+$('.send').click(function () {
+    $('.send_box').toggleClass('show')
     $(this).toggleClass('btn_active')
 
-    if (!$('.qr_box').hasClass('hide')) {
-        $('.qr_box').toggleClass('hide')
-    }
-    if (!$(this).hasClass('btn_active')) {
-        $('.qr_box').removeClass('hide')
-    }
+    //    if (!$('.qr_box').hasClass('hide')) {
+    //        $('.qr_box').toggleClass('hide')
+    //    }
+    //    if (!$(this).hasClass('btn_active')) {
+    //        $('.qr_box').removeClass('hide')
+    //    }
+
+    $('.qr_box').addClass('hide')
+    $('.function_btn_box').addClass('hide')
 
     $('.trade_box').removeClass('show')
+    $('.history_box').removeClass('show')
+    $('.keys_box').removeClass('show')
+
     $('.trade').removeClass('btn_active')
+    $('.history').removeClass('btn_active')
+    $('.keys').removeClass('btn_active')
 })
+
+$('.send_btn').click(function () {
+    $('.send_view').addClass('hide')
+    $('.confirm_view').removeClass('hide')
+    $('.wallet_box .box').addClass('hide')
+    $('.send_box').addClass('send_box_confirm')
+
+    var inputs = document.getElementById('send_to');
+    var progressBarOptions = {
+        value: 0.75,
+        size: 160,
+        fill: {
+            color: '#AAB8C6'
+        }
+    }
+
+    if (inputs.value == '') {
+        $('.loading_content').removeClass('hide')
+        $('#circle-c').circleProgress({
+            value: 1,
+            size: 160,
+            fill: {
+                color: '#2FCBAE'
+            }
+        });
+        setTimeout(function () {
+            $('.loading_content').addClass('hide')
+            $('.failed_img').removeClass('hide')
+        }, 1200)
+
+    } else {
+        $('.loading_content').removeClass('hide')
+        $('#circle-c').circleProgress({
+            value: 1,
+            size: 160,
+            fill: {
+                color: '#2FCBAE'
+            }
+        });
+        setTimeout(function () {
+            $('.loading_content').addClass('hide')
+            $('.confirm_img').removeClass('hide')
+
+        }, 1200)
+
+    }
+    inputs.value = ''
+
+})
+
+$('.return_btn').click(function () {
+    $('.qr_box').removeClass('hide')
+    $('.function_btn_box').removeClass('hide')
+
+    $('.send_box').toggleClass('show')
+    $('.send').removeClass('btn_active')
+
+    $('.send_view').removeClass('hide')
+    $('.confirm_view').addClass('hide')
+
+    $('.failed_img').addClass('hide')
+    $('.confirm_img').addClass('hide')
+
+    $('.wallet_box .box').removeClass('hide')
+    $('.send_box').removeClass('send_box_confirm')
+    $('.mobile_send').removeClass('btn_active')
+})
+
+$('.cancel_btn').click(function () {
+    $('.qr_box').removeClass('hide')
+    $('.function_btn_box').removeClass('hide')
+
+    $('.send_box').toggleClass('show')
+    $('.send').toggleClass('btn_active')
+    $('.mobile_send').removeClass('btn_active')
+})
+
 $('.trade').click(function () {
     $('.trade_box').toggleClass('show')
     $(this).toggleClass('btn_active')
@@ -63,31 +147,129 @@ $('.trade').click(function () {
         $('.qr_box').removeClass('hide')
     }
 
+    $('.send_box').removeClass('show')
+    $('.history_box').removeClass('show')
+    $('.keys_box').removeClass('show')
+
+    $('.send').removeClass('btn_active')
     $('.history').removeClass('btn_active')
-
-
+    $('.keys').removeClass('btn_active')
 })
+
+$('.history').click(function () {
+    $('.history_box').toggleClass('show')
+    $(this).toggleClass('btn_active')
+
+    if (!$('.qr_box').hasClass('hide')) {
+        $('.qr_box').toggleClass('hide')
+    }
+    if (!$(this).hasClass('btn_active')) {
+        $('.qr_box').removeClass('hide')
+    }
+
+    $('.send_box').removeClass('show')
+    $('.trade_box').removeClass('show')
+    $('.keys_box').removeClass('show')
+
+    $('.send').removeClass('btn_active')
+    $('.trade').removeClass('btn_active')
+    $('.keys').removeClass('btn_active')
+})
+
+$('.keys').click(function () {
+    $('.keys_box').toggleClass('show')
+    $(this).toggleClass('btn_active')
+
+    if (!$('.qr_box').hasClass('hide')) {
+        $('.qr_box').toggleClass('hide')
+    }
+    if (!$(this).hasClass('btn_active')) {
+        $('.qr_box').removeClass('hide')
+    }
+
+    $('.send_box').removeClass('show')
+    $('.trade_box').removeClass('show')
+    $('.history_box').removeClass('show')
+
+    $('.send').removeClass('btn_active')
+    $('.trade').removeClass('btn_active')
+    $('.history').removeClass('btn_active')
+})
+
+
+
 
 
 //mobile
-$('.mobile_history').click(function () {
-    $(this).toggleClass('btn_active')
-
+$('.mobile_send').click(function () {
+    $(this).addClass('btn_active')
+    $('.send_box').addClass('show')
+    $('.wallet_box .box').removeClass('hide')
+    $('.send_view').removeClass('hide')
+    $('.confirm_view').addClass('hide')
 
     $('.qr_box').addClass('hide')
 
+    $('.trade_box').removeClass('.show')
+    $('.history_box').removeClass('show')
+    $('.keys_box').removeClass('show')
 
-    $('.trade_box').removeClass('show')
     $('.mobile_trade').removeClass('btn_active')
+    $('.mobile_history').removeClass('btn_active')
+    $('.mobile_keys').removeClass('btn_active')
+    $('.navbar-toggler').toggleClass('active');
 })
 
 $('.mobile_trade').click(function () {
-    $(this).toggleClass('btn_active')
+    $(this).addClass('btn_active')
+    $('.trade_box').addClass('show')
+    $('.wallet_box .box').removeClass('hide')
 
 
     $('.qr_box').addClass('hide')
+
+    $('.send_box').removeClass('show')
+    $('.history_box').removeClass('show')
+    $('.keys_box').removeClass('show')
+
+    $('.mobile_send').removeClass('btn_active')
     $('.mobile_history').removeClass('btn_active')
+    $('.mobile_keys').removeClass('btn_active')
+    $('.navbar-toggler').toggleClass('active');
+
+})
+
+$('.mobile_history').click(function () {
+    $(this).addClass('btn_active')
+    $('.history_box').addClass('show')
+    $('.wallet_box .box').removeClass('hide')
 
 
-    $('.trade_box').toggleClass('show')
+    $('.qr_box').addClass('hide')
+
+    $('.send_box').removeClass('show')
+    $('.trade_box').removeClass('show')
+    $('.keys_box').removeClass('show')
+
+    $('.mobile_send').removeClass('btn_active')
+    $('.mobile_trade').removeClass('btn_active')
+    $('.mobile_keys').removeClass('btn_active')
+    $('.navbar-toggler').toggleClass('active');
+})
+
+$('.mobile_keys').click(function () {
+    $(this).addClass('btn_active')
+    $('.keys_box').addClass('show')
+    $('.wallet_box .box').removeClass('hide')
+
+    $('.qr_box').addClass('hide')
+
+    $('.send_box').removeClass('show')
+    $('.trade_box').removeClass('show')
+    $('.history_box').removeClass('show')
+
+    $('.mobile_send').removeClass('btn_active')
+    $('.mobile_trade').removeClass('btn_active')
+    $('.mobile_history').removeClass('btn_active')
+    $('.navbar-toggler').toggleClass('active');
 })
